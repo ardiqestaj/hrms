75626a4a6b060c81fffd6ea097c703bd5eb1322e
Ardi Qestaj <ardiqestaj@gmail.com>
2022-01-24 00:42:45 +0100

    First commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+	<meta name="description" content="SoengSouy Admin Template">
+	<meta name="keywords" content="admin, estimates, bootstrap, business, corporate, creative, management, minimal, modern, accounts, invoice, html5, responsive, CRM, Projects">
+	<meta name="author" content="SoengSouy Admin Template">
+	<meta name="robots" content="noindex, nofollow">
+	<title>Dashboard - HRMS</title>
+	<!-- Favicon -->
+	<link rel="shortcut icon" type="image/x-icon" href="{{ URL::to('assets/img/favicon.png') }}">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="{{ URL::to('assets/css/bootstrap.min.css') }}">
+	<!-- Fontawesome CSS -->
+	<link rel="stylesheet" href="{{ URL::to('assets/css/font-awesome.min.css') }}">
+	<!-- Lineawesome CSS -->
+	<link rel="stylesheet" href="{{ URL::to('assets/css/line-awesome.min.css') }}">
+	<!-- Datatable CSS -->
+	<link rel="stylesheet" href="{{ URL::to('assets/css/dataTables.bootstrap4.min.css') }}">
+	<!-- Select2 CSS -->
+	<link rel="stylesheet" href="{{ URL::to('assets/css/select2.min.css') }}">
+	<!-- Datetimepicker CSS -->
+	<link rel="stylesheet" href="{{ URL::to('assets/css/bootstrap-datetimepicker.min.css') }}">
+	<!-- Chart CSS -->
+	<link rel="stylesheet" href="{{ URL::to('ssets/plugins/morris/morris.css') }}">
+	<!-- Main CSS -->
+	<link rel="stylesheet" href="{{ URL::to('assets/css/style.css') }}">
+	<link rel="stylesheet" href="{{ URL::to('css/app.css') }}">
+
+
+	{{-- message toastr --}}
+	<link rel="stylesheet" href="{{ URL::to('assets/css/toastr.min.css') }}">
+	<script src="{{ URL::to('assets/js/toastr_jquery.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/toastr.min.js') }}"></script>
+</head>
+
+<body>
+	<style>    
+		.invalid-feedback{
+			font-size: 14px;
+		}
+	</style>
+	<!-- Main Wrapper -->
+	<div class="main-wrapper">
+		
+	@include('navheader.nav')
+		
+		<!-- /Header -->
+		<!-- Sidebar -->
+		{{-- @yield('menu') --}}
+		@include('sidebar.sidebar')
+		<!-- /Sidebar -->
+		<!-- Page Wrapper -->
+		@yield('content')
+		<!-- /Page Wrapper -->
+	</div>
+	<!-- /Main Wrapper -->
+
+	<!-- jQuery -->
+	<script src="{{ URL::to('assets/js/jquery-3.5.1.min.js') }}"></script>
+	<!-- Bootstrap Core JS -->
+	<script src="{{ URL::to('assets/js/popper.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/bootstrap.min.js') }}"></script>
+	<!-- Chart JS -->
+	<script src="{{ URL::to('assets/plugins/morris/morris.min.js') }}"></script>
+	<script src="{{ URL::to('assets/plugins/raphael/raphael.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/chart.js') }}"></script>
+	<!-- Slimscroll JS -->
+	<script src="{{ URL::to('assets/js/jquery.slimscroll.min.js') }}"></script>
+	<!-- Select2 JS -->
+	<script src="{{ URL::to('assets/js/select2.min.js') }}"></script>
+	<!-- Datetimepicker JS -->
+	<script src="{{ URL::to('assets/js/moment.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/bootstrap-datetimepicker.min.js') }}"></script>
+	<!-- Datatable JS -->
+	<script src="{{ URL::to('assets/js/jquery.dataTables.min.js') }}"></script>
+	<script src="{{ URL::to('assets/js/dataTables.bootstrap4.min.js') }}"></script>
+	<!-- Multiselect JS -->
+	<script src="{{ URL::to('assets/js/multiselect.min.js') }}"></script>		
+	<!-- Custom JS -->
+	<script src="{{ URL::to('assets/js/app.js') }}"></script>
+	
+	@yield('script')
+</body>
+</html>
